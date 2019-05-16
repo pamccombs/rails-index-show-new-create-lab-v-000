585b4ae84c4ee46910d0d1d5b33c3057f3c43258
Patrick McCombs <pa.mccombs1@gmail.com>
2019-05-16 22:44:23 +0000

    Automatically backed up by Learn

--- db/migrate/20190516224336_create_coupons.rb
+++ db/migrate/20190516224336_create_coupons.rb
@@ -3,8 +3,6 @@ class CreateCoupons < ActiveRecord::Migration[5.0]
     create_table :coupons do |t|
       t.string :coupon_code
       t.string :store
-
-      t.timestamps null: false
     end
   end
 end
